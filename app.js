32e44d19d673713e98ffd4d233928757da34ad3f
XDobriev <XDobriev@yandex.ru>
2024-08-26 23:06:08 +0300

    3.5. Быстрое создание Map

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 'use strict'
 
-const weatherMap = new Map();
-weatherMap
-    .set('London', '10')
-    .set('Moscow', '7')
-
-console.log(weatherMap.get('Moscow'));
-console.log(weatherMap.get('Mosco'));
-console.log(weatherMap.has('Mosco'));
-console.log(weatherMap.has('Moscow'));
-/* console.log(weatherMap.delete('Moscow')); */
-
-const arr = [1, 2, 3]
-weatherMap
-    .set(11, 5)
-    .set(true, 'yes')
-    .set(false, 'no')
-    .set(arr, 'array')
-    .set({a: 1}, {b: 1})
+const weatherMap = new Map([
+    ['London', '10'],
+    ['Moscow', '7']
+])
 console.log(weatherMap);
-console.log(weatherMap.size);
-console.log(weatherMap.get(arr));
-console.log(weatherMap.get(arr));
+
+const weatherObject = {
+    london: 10,
+    moscow: 7,
+    paris: 14
+}
+
+console.log(Object.entries(weatherObject));
+const weatherMap2 = new Map(Object.entries(weatherObject))
+console.log(weatherMap2);
+console.log(weatherMap2.size);
