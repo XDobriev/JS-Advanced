cfea6db8fe7c881b3ea5bfe84625c8c09040225c
XDobriev <XDobriev@yandex.ru>
2024-09-09 22:14:18 +0300

    6.1. Создание таймеров

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 'use strict'
 
-const date = new Date();
-console.log(date);
-console.log(new Intl.DateTimeFormat('ru-Ru').format(date));
-const options1 = {
-    hour: 'numeric',
-    minute: 'numeric'
-}
-console.log(new Intl.DateTimeFormat('ru-Ru', options1).format(date));
-const options2 = {
-    hour: 'numeric',
-    minute: 'numeric',
-    month: 'long',
-    weekday: 'short',
-    year: '2-digit'
-}
-console.log(new Intl.DateTimeFormat('en-US', options2).format(date));
-console.log(new Intl.DateTimeFormat('ru-RU', options2).format(date));
+console.log(1)
 
-console.log(navigator.language)
-console.log(new Intl.DateTimeFormat(navigator.language, options1).format(date));
+const messages = ['Bash', 'Boom!'];
+
+
+const boomTimer = setTimeout((message1, message2) => {
+    console.log(2);
+    console.log(message1);
+    console.log(message2);
+}, 2000, ...messages)
+
+// clearTimeout(boomTimer)
+
+console.log(3);
