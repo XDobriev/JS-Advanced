e64d9b57f329a9a6e2d9870ed87f60ea745d402b
XDobriev <XDobriev@yandex.ru>
2024-09-17 16:47:28 +0300

    7.3. Оператор new

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 'use strict'
-//Реализация ООП в JS
-const a = [1];
-console.dir(a);
+//Оператор new
+const User = function(email, password) {
+  this.email = email;
+  this.password = password + '###';
+}
+
+const user1 = new User('a@a.ru', '123');
+console.log(user1);
+const user2 = new User('a2@a.ru', '123');
+console.log(user2);
+console.log(user2 instanceof User);
+
+// создается пустой объект 
+// вызывается User функция
+// this = пустому объекту
+// объект связывается с prototype
+// возвращается объект
